8333716888dfd1e37f57be8da88ac3288eead5c3
Jeremiah Risinger <risinger.jeremiah@gmail.com>
2020-04-25 15:09:26 -0500

    TrackBuilder pseudocode in place

--- Assets/Generation/TrackBuilder.cs
+++ Assets/Generation/TrackBuilder.cs
@@ -12,11 +12,24 @@ public class TrackBuilder : MonoBehaviour
     void Start()
     {
         track = new GameObject[trackLength];
+        BuildTrack();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    void BuildTrack()
+    { 
+        //for all in track[]
+            //randomly instantiate track piece
+            //do more straight than turn
+                // 4:1 OR 3:1 
+            //if a turn 
+                //randomly decide right or left turn
+        //for loop done
+
+        //return
     }
 }
